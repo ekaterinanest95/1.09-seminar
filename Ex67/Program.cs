55784a29e4f30ec3b03c4fcbc9b260714ae74303
Ekaterina <katya.kinal@gmail.com>
2022-09-01 21:26:52 +0300

    Ex67

---
+++ Ex67/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. 453 -> 12 45 -> 9
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigits (int n)
+{
+    if(n == 0) return 0;
+
+    return n % 10 + SumOfDigits(n / 10);
+}
+Console.WriteLine(SumOfDigits(number));
