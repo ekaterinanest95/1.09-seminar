28810b02df871a0d6fa6ab89dc1ab5c8a12685a8
Ekaterina <katya.kinal@gmail.com>
2022-09-02 07:03:22 +0300

    Ex65

--- Ex65/Program.cs
+++ Ex65/Program.cs
@@ -2,16 +2,15 @@
 //M = 1; N = 5 -> "1, 2, 3, 4, 5"
 //M = 4; N = 8 -> "4, 6, 7, 8"
 
-
-int N = Convert.ToInt32(Console.ReadLine());
 int M = Convert.ToInt32(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
 
-if (M > N) return "Ошибка";
+//if (M > N) return "Ошибка";
 
 string PrintNumbers(int start, int end)
 {
     if (start == end) return start.ToString();
-    return start + ", " + PrintNumbers(start + 1, end);
+    return (start + ", " + PrintNumbers(start + 1, end));
 }
 
 Console.WriteLine(PrintNumbers(M, N));
